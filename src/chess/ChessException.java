3bcbfb5c501b1c89679d26056c32643bc7520b28
rogeriolana <119761642+rogeriolana@users.noreply.github.com>
2023-10-24 22:43:01 -0300

    Moving Pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessException extends RuntimeException {
+import boardgame.BoardExcepition;
+
+public class ChessException extends BoardExcepition {
 	private static final long serialVersionUID = 1L;
 
 	public ChessException(String msg) {
