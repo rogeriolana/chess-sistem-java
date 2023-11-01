0d74f78b6556690d72369ce5c9b2aa42d9e3fda2
rogeriolana <119761642+rogeriolana@users.noreply.github.com>
2023-11-01 15:57:37 -0300

    Implement possible moves Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,11 +29,17 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
-
 	}
-	
+
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can`t move to target position");
+		}
+	}
+
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
@@ -42,14 +48,14 @@ public class ChessMatch {
 	}
 
 	private void validateSourcePosition(Position position) {
-		if (!board.thereIsAPiece(position)) {	
-			 throw new ChessException("There is no piece on source position");
+		if (!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
 		}
-		if (!board.piece(position). isThereAnyPossibleMove()) {
+		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for chosen piece");
 		}
 
-	} 
+	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,8 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
+import boardgame.Position;
 
 public abstract  class ChessPiece extends Piece {
 
@@ -14,6 +16,12 @@ public abstract  class ChessPiece extends Piece {
 
 	public Color getColor() {
 		return color;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+		
 	}
 
 }
